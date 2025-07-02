6791597ca74c92f4f43eecca60ac15638adb17a6
remotelogin <fluffshot@proton.me>
2025-07-03 00:17:44 +0200

    serialized up

--- main.cpp
+++ main.cpp
@@ -1,16 +1,76 @@
 #include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+#include <util/setbaud.h>
+
+void uart_init(void) {
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+}
+
+int uart_putchar(char c, FILE *stream) {
+    if (c == '\n') {
+        uart_putchar('\r', stream);  // send carriage return before newline
+    }
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+    return 0;
+}
+
+int uart_getchar(FILE *stream) {
+    loop_until_bit_is_set(UCSR0A, RXC0);
+    return UDR0;
+}
+
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE uart_input  = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+
 int main() {
+
+  //setup uart
+  uart_init();
+  stdout = &uart_output;
+  stdin  = &uart_input;
+
+  char input;
+
+  //setup io
   DDRB &= ~(1 << PB5); 
   DDRB |= (1 << PB4);
+
+  //main loop
   while (1) {
+
+    //check if D12 is high
     if(PINB & (1 << PB4)){
       PORTB = PORTB ^ (1 << PB5);
     }
     else {
       PORTB = PORTB ^ ~(1 << PB5);
     }
-    _delay_ms(5);
+
+    puts("Hello world!");
+    printf("Hello world!");
+    
+    //make cpu not die
+    _delay_ms(100);
   }
 }
